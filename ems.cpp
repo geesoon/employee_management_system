45f98bb0d017d927e97e03791ba1f55ccb7111a1
jonesyap97 <jonesyap1997@hotmail.com>
2018-12-06 00:12:27 +0800

    Add class method
    Add method to Performance class

--- ems.cpp
+++ ems.cpp
@@ -14,18 +14,72 @@ struct details{
     string cv;
 };
 
+struct Goal{
+	
+	string goal_desc;
+	float progress;
+};
+
 class payroll{
 
 };
 
-class performance{
-
+class Performance{
+	private:
+		Goal goal[size];
+		int goal_cnt;
+	public:
+		Performance() {goal_cnt = 0;}
+		
+		void createGoal()
+		{
+			string temp;
+			cout<<"Please enter the goal description.\n";
+			getline(cin,temp);
+			goal[goal_cnt].goal_desc=temp;
+			goal[goal_cnt].progress=0;	
+		}
+		void deleteGoal();
+		void printGoal()
+		{
+			float tot=0;
+			for(int i=0;i<goal_cnt;i++)
+			{
+				cout<<goal[i].goal_desc<<" \t\t "<<goal[i].progress<<endl;
+				tot+=goal[i].progress;
+			}
+			
+			cout<<"Overall progess score = "<< (tot/goal_cnt)<<endl;
+		}
+		void updateProgress()
+		{
+			int index,newscore;
+			
+			cout<<"Enter goal index >> "; cin>>index;
+			
+			if(index > goal_cnt || index < 0)
+			{
+				cout<<"Error, index out of bound\n";
+				return;
+			}
+			
+			cout<<"Enter new progress score >> "; cin>>newscore;
+			
+			if(newscore < 0 || newscore > 100)
+			{
+				cout<<"Error, input out of bound\n";
+				return ;
+			}
+			
+			cout<<"New score update sucessful.\n";
+			
+		}
 };
 
 class employee{
     private:
         payroll proll;
-        performance perform;
+        Performance perform;
         details info;
     public:
 		void setinfo(details *file_info)
